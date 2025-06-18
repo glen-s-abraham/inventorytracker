5fbf4b920d79bb9553245c0e6aee46a051383866
iss211 <glen.abraham@immcossi.in>
2025-06-18 10:41:24 +0530

    initial commit

---
+++ src/test/java/in/mariasorganics/inventorytracker/InventorytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mariasorganics.inventorytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
